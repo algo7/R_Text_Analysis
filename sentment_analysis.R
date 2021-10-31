cc346d6b1ed9705f7f4147324accc3ba24c9164e
algo7 <11154774+algo7@users.noreply.github.com>
2021-10-31 17:45:50 +0100

    add libs

--- sentment_analysis.R
+++ sentment_analysis.R
@@ -1,3 +1,23 @@
+# List of required pacakges
+required_pkgs <- c(
+    "tm"
+)
+
+# Empty list to hold dependencies that are not isntalled
+not_met_dependencies <- c()
+
+# Check if required packages are installed
+for (pkg in required_pkgs) {
+    if (!(pkg %in% rownames(installed.packages()))) {
+        not_met_dependencies <- c(not_met_dependencies, pkg)
+    }
+}
+
+# Install missing packages
+if (length(not_met_dependencies) != 0) {
+    install.packages(not_met_dependencies)
+}
+
 # Load dataset
 data <- read.csv("./sample_tweets.csv", header = T)
 
