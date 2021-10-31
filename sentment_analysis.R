ba75bf8096b7b71a97b82b48ae25ddeabb2fbdae
algo7 <11154774+algo7@users.noreply.github.com>
2021-10-31 18:49:50 +0100

    text pre processing

--- sentment_analysis.R
+++ sentment_analysis.R
@@ -25,4 +25,69 @@ library("tm")
 data <- read.csv("./sample_tweets.csv", header = T)
 
 # Print the structure of the dataset
-str(data)
+# str(data)
+
+# Extract the text column
+corpus <- iconv(data$text)
+
+# Load the text as a corpus
+corpus <- Corpus(VectorSource(corpus))
+
+# Text transformation / pre-processing
+# Function to substitute the given pattern with a white space
+to_space <- content_transformer(function(text, pattern) gsub(pattern, "", text))
+
+# Url pattern
+url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+
+# Remove orphan alphabets (one alphabet with space before and after)
+remove_orphan_alphabet <- content_transformer(function(text) gsub("\\s[a-z]\\s", " ", text))
+
+# Function to substitute url to white space
+remove_url <- content_transformer(function(text) gsub(url_pattern, " ", text))
+
+# Function to substitute url user name to white space
+remove_user_name <- content_transformer(function(text) gsub("@(\\w+):", " ", text))
+
+# Convert the text to lower case
+corpus <- tm_map(corpus, content_transformer(tolower))
+
+# Remove urls
+corpus <- tm_map(corpus, remove_url)
+
+# Remove urls
+corpus <- tm_map(corpus, remove_user_name)
+
+# Remove /
+corpus <- tm_map(corpus, to_space, "/")
+
+# Remove @
+corpus <- tm_map(corpus, to_space, "@")
+
+# Remove |
+corpus <- tm_map(corpus, to_space, "\\|")
+
+# Remove $
+corpus <- tm_map(corpus, to_space, "\\$")
+
+# Remove numbers
+corpus <- tm_map(corpus, removeNumbers)
+
+# Remove rt, which is appended to the beginning of the tweet
+corpus <- tm_map(corpus, removeWords, c("rt"))
+
+# Remove english common stopwords
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+
+# Remove punctuations
+corpus <- tm_map(corpus, removePunctuation)
+
+# Remove urls
+corpus <- tm_map(corpus, remove_orphan_alphabet)
+
+# Eliminate extra white spaces
+corpus <- tm_map(corpus, stripWhitespace)
+
+
+
+inspect(corpus[1:5])
