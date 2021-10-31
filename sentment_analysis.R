f089417f90317c9a19b7508d3526ea527623e5f4
algo7 <11154774+algo7@users.noreply.github.com>
2021-10-31 21:07:54 +0100

    parallelization and plotting

--- sentment_analysis.R
+++ sentment_analysis.R
@@ -1,6 +1,6 @@
 # List of required pacakges
 required_pkgs <- c(
-    "tm"
+    "tm", "syuzhet", "parallel"
 )
 
 # Empty list to hold dependencies that are not isntalled
@@ -20,6 +20,18 @@ if (length(not_met_dependencies) != 0) {
 
 # Load packages
 library("tm")
+library("syuzhet")
+library("parallel")
+
+## Loading required package: parallel
+
+# Creates a set of copies of R running
+# in parallel and communicating over sockets.
+cl <- makeCluster(detectCores() - 1)
+
+# Parallelize the following functions
+clusterExport(cl = cl, c("get_sentiment", "get_sent_values", "get_nrc_sentiment", "get_nrc_values", "parLapply"))
+
 
 # Load dataset
 data <- read.csv("./sample_tweets.csv", header = T)
@@ -31,7 +43,74 @@ data <- read.csv("./sample_tweets.csv", header = T)
 docs <- iconv(data$text)
 
 # Load the text as a corpus
-corpus <- Corpus(VectorSource(docs))
+corpus <- VCorpus(VectorSource(docs))
+
+# Text transformation / pre-processing
+# Function to substitute the given pattern with a white space
+to_space <- content_transformer(function(text, pattern) gsub(pattern, "", text))
+
+# Url pattern
+url_pattern <- "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+
+# Remove orphan alphabets (one alphabet with space before and after)
+remove_orphan_alphabet <- content_transformer(function(text) gsub("\\s[a-z]\\s", " ", text))
+
+# Function to substitute url to white space
+remove_url <- content_transformer(function(text) gsub(url_pattern, " ", text))
+
+# Function to substitute url user name to white space
+remove_user_name <- content_transformer(function(text) gsub("@(\\w+):", " ", text))
+
+# Function to substitute non-alpha-num chars to white space
+remove_special_chars <- content_transformer(function(text) gsub("[^a-z ]", " ", text))
+
+# Convert the text to lower case
+corpus <- tm_map(corpus, content_transformer(tolower))
+
+# Remove urls
+corpus <- tm_map(corpus, remove_url)
+
+# Remove urls
+corpus <- tm_map(corpus, remove_user_name)
+
+# Remove all non-aphabet-numeric characters
+corpus <- tm_map(corpus, remove_special_chars)
+
+# Remove rt, which is appended to the beginning of the tweet
+corpus <- tm_map(corpus, removeWords, c("rt"))
+
+# Remove english common stopwords
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
+
+# Remove appl & apple => name of the stock
+corpus <- tm_map(corpus, removeWords, c("aapl", "apple"))
+
+# Remove urls
+corpus <- tm_map(corpus, remove_orphan_alphabet)
+
+# Eliminate extra white spaces
+corpus <- tm_map(corpus, stripWhitespace)
+
+
+
+# Build term-document matrix
+# Document matrix is a table containing the frequency of the words.
+# Column names are words and row names are documents
+dtm <- TermDocumentMatrix(corpus)
+
+# Convert term doc matrix into matrix
+m <- as.matrix(dtm)
+# Load dataset
+data <- read.csv("./sample_tweets.csv", header = T)
+
+# Print the structure of the dataset
+# str(data)
+
+# Extract the text column
+docs <- iconv(data$text)
+
+# Load the text as a corpus
+corpus <- VCorpus(VectorSource(docs))
 
 # Text transformation / pre-processing
 # Function to substitute the given pattern with a white space
@@ -102,3 +181,63 @@ barplot(word_freq,
     # Gradient
     col = rainbow(50)
 )
+
+
+# Uses National Research Council Canada (NRC)  Emotion lexicon
+# with eight emotions (anger, fear, anticipation, trust, surprise, sadness, joy, and disgust)
+# and two sentiments (negative and positive)
+sentiment_scores <- get_nrc_sentiment(docs, language = "english")
+
+# Sum the sentiment score matrix
+sentiment_sum <- colSums(sentiment_scores)
+
+# Plot it
+barplot(sentiment_sum,
+    las = 2,
+    col = rainbow(10),
+    ylab = "Count",
+    main = "Sentiment Scores Tweets",
+    ylim = c(0, max(sentiment_sum) + 5)
+)
+
+# Extract only the words with frequency greater than 25
+word_freq <- subset(word_freq, word_freq >= 25)
+
+# PDF settings
+par(mfrow = c(1, 2))
+pdf("sent.pdf", width = 12, height = 8, compress = F)
+
+
+
+# Plot it
+sent1 <- barplot(word_freq,
+    # axis lables perpendicular to the bars
+    las = 2,
+    # Gradient
+    col = rainbow(50)
+)
+
+
+# Uses National Research Council Canada (NRC)  Emotion lexicon
+# with eight emotions (anger, fear, anticipation, trust, surprise, sadness, joy, and disgust)
+# and two sentiments (negative and positive)
+sentiment_scores <- get_nrc_sentiment(docs, language = "english")
+
+# Sum the sentiment score matrix
+sentiment_sum <- colSums(sentiment_scores)
+
+# Plot it
+sent2 <- barplot(sentiment_sum,
+    las = 2,
+    col = rainbow(10),
+    ylab = "Count",
+    main = "Sentiment Scores Tweets",
+    ylim = c(0, max(sentiment_sum) + 5)
+)
+
+# Turn of the current graphic device after creating the plot
+# to finish creating the image file
+dev.off()
+
+# Stop the cluster
+stopCluster(cl)
