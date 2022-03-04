9c6d045e07cc69851a034f885513682d4004f98d
algo7 <11154774+algo7@users.noreply.github.com>
2022-03-04 08:01:16 +0100

    folder structure changes + linting

--- WordCloud.R
+++ WordCloud.R
@@ -27,7 +27,7 @@ library("wordcloud2")
 library("RColorBrewer")
 
 # Load Data
-data <- read.csv("reviews.csv",header = T)
+data <- read.csv("reviews.csv", header = T)
 
 # Convert the raw data to a data frame
 data <- as.data.frame(data)
@@ -71,14 +71,15 @@ docs <- tm_map(docs, removePunctuation)
 
 # Remove your own stop word
 # specify your stopwords as a character vector
-docs <- tm_map(docs, removeWords, c("hotel",
-                                    "staff","room",
-                                    "lake","rooms",
-                                    "one","service",
-                                    "palace","breakfast",
-                                    "lausanne","just","also","can",
-                                    "every"
-                                    ))
+docs <- tm_map(docs, removeWords, c(
+  "hotel",
+  "staff", "room",
+  "lake", "rooms",
+  "one", "service",
+  "palace", "breakfast",
+  "lausanne", "just", "also", "can",
+  "every"
+))
 
 # Build term-document matrix
 # Document matrix is a table containing the frequency of the words.
@@ -97,16 +98,16 @@ set.seed(2645)
 
 
 # For the the graph (1 row, 2 cols)
-#par(mfrow = c(1, 2))
+# par(mfrow = c(1, 2))
 
 # WC Plot
 wc <- wordcloud(
-  words = d$word, 
-  freq = d$freq, 
+  words = d$word,
+  freq = d$freq,
   min.freq = 50,
-  max.words = 200, 
+  max.words = 200,
   # Graphic Stuff
-  random.order = FALSE, 
+  random.order = FALSE,
   rot.per = 0.35,
   colors = brewer.pal(12, "Set3")
 )
@@ -114,18 +115,16 @@ wc <- wordcloud(
 
 # Create a histogram of the top 20 most frequent words
 top30_word_histo <- barplot(d[1:30, ]$freq,
-                            las = 2, names.arg = d[1:30, ]$word,
-                            col = "lightblue", main = "Top 10 Most Frequent Words",
-                            ylab = "Frequencies", ylim = c(0, max(d$freq) + 5), # yaxp = c(0, max(d$freq) + 5, 10)
+  las = 2, names.arg = d[1:30, ]$word,
+  col = "lightblue", main = "Top 10 Most Frequent Words",
+  ylab = "Frequencies", ylim = c(0, max(d$freq) + 5), # yaxp = c(0, max(d$freq) + 5, 10)
 )
 
 # Find words that appear more than 4 times
 print(findFreqTerms(dtm, lowfreq = 50))
 
 # Find terms that are associated with "freedom" with a correlation of > 0.3
-print(findAssocs(dtm, terms = "lausanne", corlimit = 0.1))
+print(findAssocs(dtm, terms = "good", corlimit = 0.1))
 
 # Wordcloud2 support, needs to be printed explicitly
-#wc2 <- wordcloud2(data = d, size = 1.6, color = "random-dark")
-
-
+# wc2 <- wordcloud2(data = d, size = 1.6, color = "random-dark")
