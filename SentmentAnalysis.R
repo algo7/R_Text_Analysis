e9bc23598bad2d5e789131965c18921de1e5e47b
algo7 <11154774+algo7@users.noreply.github.com>
2022-10-05 00:47:58 +0200

    Formatting

--- SentmentAnalysis.R
+++ SentmentAnalysis.R
@@ -1,8 +1,12 @@
-# Set seed => all graphs will look the same given the same input
-set.seed(2645)
+################################
+#                             ##
+#    Install Packages         ##
+#                             ##
+################################
+
 # List of required packages
 required_pkgs <- c(
-    "tm", "syuzhet"
+  "tm", "syuzhet"
 )
 
 # Empty list to hold dependencies that are not installed
@@ -10,58 +14,70 @@ not_met_dependencies <- c()
 
 # Check if required packages are installed
 for (pkg in required_pkgs) {
-    if (!(pkg %in% rownames(installed.packages()))) {
-        not_met_dependencies <- c(not_met_dependencies, pkg)
-    }
+  if (!(pkg %in% rownames(installed.packages()))) {
+    not_met_dependencies <- c(not_met_dependencies, pkg)
+  }
 }
 
 # Install missing packages
 if (length(not_met_dependencies) != 0) {
-    install.packages(not_met_dependencies)
+  install.packages(not_met_dependencies)
 }
 
 # Load packages
 library("tm")
 library("syuzhet")
 
-######## Start Here ##########
+
+################################
+#                             ##
+#          START HERE         ##
+#                             ##
+################################
 
 
-######## Graphs ##########
-# Bar plots of word frequencies
+################################
+#                             ##
+#    Graphic Settings         ##
+#                             ##
+################################
+# Plot it
 sent1 <- barplot(
-    height= word_freq,
-    main = "Word Frequencies",
-    ylab = "Count",
-    names.arg = names(word_freq),
-    # Graphic stuff
-    # Space between axis labels perpendicular to the bars
-    las = 2,
-    # Gradient
-    col = rainbow(50),
-    ylim = c(0, max(word_freq) * 1.1),
+  height = word_freq,
+  main = "Word Frequencies",
+  ylab = "Count",
+  names.arg = names(word_freq),
+  # Space between axis labels perpendicular to the bars
+  las = 2,
+  # Gradient
+  col = rainbow(50),
+  ylim = c(0, max(word_freq) * 1.1),
 )
 
 # Add actual value on top of the bars
-text(sent1, 
-     word_freq, 
-     labels = word_freq, 
-     pos = 3, 
-     cex = 0.7)
+text(sent1, word_freq, labels = word_freq, pos = 3, cex = 0.7)
 
+# Uses National Research Council Canada (NRC)  Emotion lexicon
+# with eight emotions (anger, fear, anticipation, trust, surprise, sadness, joy, and disgust)
+# and two sentiments (negative and positive)
+sentiment_scores <- get_nrc_sentiment(names(word_freq), language = "english")
 
+# Sum the sentiment score matrix
+sentiment_sum <- colSums(sentiment_scores)
+sentiment_sum<-sort(sentiment_sum,decreasing = TRUE)
 # Plot it
 sent2 <- barplot(sentiment_sum,
-    ylab = "Count",
-    main = "Reviews Sentiment Scores",
-    # Graphic stuff
-    col = rainbow(10),
-    las = 2,
-    ylim = c(0, max(sentiment_sum) * 1.1)
+                 las = 2,
+                 col = rainbow(10),
+                 ylab = "Count",
+                 main = "Sentiment Scores Comment",
+                 ylim = c(0, max(sentiment_sum) * 1.1)
 )
 
 # Add actual value on top of the bars
-text(sent2, 
-     sentiment_sum, 
-     labels = sentiment_sum, 
-     pos = 3)
+text(sent2,
+     sentiment_sum,
+     labels = sentiment_sum,
+     pos = 3
+)
+
