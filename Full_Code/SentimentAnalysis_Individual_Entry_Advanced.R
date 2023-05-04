6782d65699eafa00ffea6a3ee727c16aaa6e4c50
algo7 <11154774+algo7@users.noreply.github.com>
2023-05-04 21:34:51 +0200

    add noun

--- Full_Code/SentimentAnalysis_Individual_Entry_Advanced.R
+++ Full_Code/SentimentAnalysis_Individual_Entry_Advanced.R
@@ -76,7 +76,7 @@ remove_undesired_pos <- content_transformer(function(text) {
   
   # Filter undesired POS
   filtered <- subset(annotation_df, 
-                     !(upos %in% c("PROPN", "VERB", "PRON", "NUM", "INTJ", "AUX", "CCONJ", "ADP", "X")))
+                     !(upos %in% c("PROPN", "NOUN", "VERB", "PRON", "NUM", "INTJ", "AUX", "CCONJ", "ADP", "X")))
   # print(paste("Filtered POS tags:", paste(filtered$upos, collapse = ", ")))
   
   
