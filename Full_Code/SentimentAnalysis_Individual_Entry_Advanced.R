325cc678002724ca7be7665f620dc0f62c756b89
algo7 <11154774+algo7@users.noreply.github.com>
2023-05-14 14:12:16 +0200

    refactoring

--- Full_Code/SentimentAnalysis_Individual_Entry_Advanced.R
+++ Full_Code/SentimentAnalysis_Individual_Entry_Advanced.R
@@ -1,6 +1,6 @@
 # List of required packages
 required_pkgs <- c(
-    "tm", "syuzhet","tidyr","ggplot2"
+  "tm", "syuzhet", "tidyr", "ggplot2"
 )
 
 # Empty list to hold dependencies that are not installed
@@ -8,14 +8,14 @@ not_met_dependencies <- c()
 
 # Check if required packages are installed
 for (pkg in required_pkgs) {
-    if (!(pkg %in% rownames(installed.packages()))) {
-        not_met_dependencies <- c(not_met_dependencies, pkg)
-    }
+  if (!(pkg %in% rownames(installed.packages()))) {
+    not_met_dependencies <- c(not_met_dependencies, pkg)
+  }
 }
 
 # Install missing packages
 if (length(not_met_dependencies) != 0) {
-    install.packages(not_met_dependencies)
+  install.packages(not_met_dependencies)
 }
 
 # Load packages
@@ -37,18 +37,18 @@ ud_model <- udpipe_load_model("/home/algo7/Desktop/code/R_Text_Analysis/english-
 path <- Sys.getenv("DATA_SOURCE_PATH")
 
 # Load data set
-if (path != ""){
+if (path != "") {
   data <- read.csv(path, header = T)
-}else{
+} else {
   file <- file.choose()
   filename <- basename(file)
   data <- read.csv(file, header = T)
-  splitted_hotel_name <- unlist(strsplit(filename," "))
+  splitted_hotel_name <- unlist(strsplit(filename, " "))
   hotel_name <- paste(splitted_hotel_name[1], splitted_hotel_name[2])
 }
 
 # Extract the text column
-docs <- iconv(data$Text)
+docs <- iconv(data$text)
 
 # Load the text as a corpus
 docs <- VCorpus(VectorSource(docs))
@@ -56,9 +56,9 @@ docs <- VCorpus(VectorSource(docs))
 # Text transformation / pre-processing
 # Function to substitute the given pattern with a white space
 to_space <- content_transformer(
-    function(text, pattern) {
-        gsub(pattern, " ", text)
-    }
+  function(text, pattern) {
+    gsub(pattern, " ", text)
+  }
 )
 
 # Function to substitute the given pattern with nothing
@@ -77,19 +77,20 @@ to_custom <- content_transformer(
 
 # Function to remove all undesired pos
 remove_undesired_pos <- content_transformer(function(text) {
-  
   # Annotate the text using the UDPipe model
   annotation <- udpipe_annotate(ud_model, text)
-  
+
   # Convert the annotation to a data frame
   annotation_df <- as.data.frame(annotation)
-  
+
   # Filter undesired POS
-  filtered <- subset(annotation_df, 
-                     !(upos %in% c("PROPN", "PRON", "NUM", "VERB", "INTJ", "AUX", "CCONJ", "ADP", "X")))
+  filtered <- subset(
+    annotation_df,
+    !(upos %in% c("PROPN", "PRON", "NUM", "VERB", "INTJ", "AUX", "CCONJ", "ADP", "X"))
+  )
   # print(paste("Filtered POS tags:", paste(filtered$upos, collapse = ", ")))
-  
-  
+
+
   # Combine the processed data into a single string
   # collapse is required other wise each non-nouns will be an individual
   # element in the character vector while a document is a string
@@ -98,19 +99,20 @@ remove_undesired_pos <- content_transformer(function(text) {
 
 # Function to adj and adv
 remove_adjective_adverb <- content_transformer(function(text) {
-  
   # Annotate the text using the UDPipe model
   annotation <- udpipe_annotate(ud_model, text)
-  
+
   # Convert the annotation to a data frame
   annotation_df <- as.data.frame(annotation)
-  
+
   # Filter undesired POS
-  filtered <- subset(annotation_df, 
-                     !(upos %in% c("ADJ","ADV")))
+  filtered <- subset(
+    annotation_df,
+    !(upos %in% c("ADJ", "ADV"))
+  )
   # print(paste("Filtered POS tags:", paste(filtered$upos, collapse = ", ")))
-  
-  
+
+
   # Combine the processed data into a single string
   # collapse is required other wise each non-nouns will be an individual
   # element in the character vector while a document is a string
@@ -134,16 +136,16 @@ docs <- tm_map(docs, removeWords, stopwords("english"))
 # Remove your own stop word
 # specify your stop words as a character vector
 docs <- tm_map(docs, removeWords, c(
-  "lake","always",
-  "one","per",
+  "lake", "always",
+  "one", "per",
   "palace",
   "just", "also", "can",
-  "every","although","get",
-  "even","will","radissons",
-  "radisson","rivage","bolongo",
-  "back","thomas","got","elysian","Emerald Beach",
-  "still","away","next","emerald","Margaritaville","margaritaville","margarita","bluebeards","bolongo bay",
-  "however","windward","passage","Windward","secret","harbour","point","dive","deep","tamarind","ritz","ferry"
+  "every", "although", "get",
+  "even", "will", "radissons",
+  "radisson", "rivage", "bolongo",
+  "back", "thomas", "got", "elysian", "Emerald Beach",
+  "still", "away", "next", "emerald", "Margaritaville", "margaritaville", "margarita", "bluebeards", "bolongo bay",
+  "however", "windward", "passage", "Windward", "secret", "harbour", "point", "dive", "deep", "tamarind", "ritz", "ferry"
 ))
 
 # Strip single english character
@@ -156,20 +158,20 @@ docs <- tm_map(docs, stripWhitespace)
 docs <- tm_map(docs, to_nothing, "^\\s+")
 
 # Convert VCorpus to data frame
-df<- data.frame(processed_text=sapply(docs, as.character))
+df <- data.frame(processed_text = sapply(docs, as.character))
 
 # Apply the get_nrc_sentiment function to the text column and create a new column
-df$sentiment <- lapply(df$text, get_nrc_sentiment)
+df$sentiment <- lapply(df$processed_text, get_nrc_sentiment)
 
 # Convert the sentiment column to separate columns
 df <- df %>%
   unnest_wider(sentiment)
 
 # Combined the raw data with analysis results
-df <- cbind(data[, 1:5],df)
+df <- cbind(data[, 1:5], df)
 
 # Save the dataframe to a CSV file
-write.csv(df, file = paste("./Debug/Stage_1_",filename, sep = ""), row.names = FALSE)
+write.csv(df, file = paste("./Debug/Stage_1_", filename, sep = ""), row.names = FALSE)
 
 # Filter out rows that have all NRC categories with 0 value
 df <- df[rowSums(df[, 7:16]) != 0, ]
@@ -179,111 +181,105 @@ df$positive <- df$joy + df$trust + df$anticipation + df$positive
 df$negative <- df$anger + df$disgust + df$fear + df$sadness + df$negative
 
 # Remove combined categories and netural category => surprise
-df <- subset(df,select = -c(trust,joy,anticipation,anger,disgust,fear,sadness,surprise))
+df <- subset(df, select = -c(trust, joy, anticipation, anger, disgust, fear, sadness, surprise))
 
 # Remove rows where positive = negative. in other words, keep those where the abs diff is > 0
 df <- subset(df, abs(positive - negative) > 0)
 
 # Function to classify text based on highest score
 classify_text <- function(row) {
-
   # Get index of column with highest score
   highest_index <- which.max(row[7:16])
-  return (names(highest_index))
+  return(names(highest_index))
 }
 
 # MARGIN 1 indicates rows, 2 indicates columns, c(1, 2) indicates rows and columns
-df$class <- apply(df, MARGIN=1, classify_text)
+df$class <- apply(df, MARGIN = 1, classify_text)
 
 # Save the dataframe to a CSV file
-write.csv(df, file = paste("./Debug/Stage_2_", filename,sep = ""), row.names = FALSE)
+write.csv(df, file = paste("./Debug/Stage_2_", filename, sep = ""), row.names = FALSE)
 
 
-graph_top_words <- function(emotion, timespan, data_source){
-  
+graph_top_words <- function(emotion, timespan, data_source) {
   emotion_lower <- tolower(emotion)
-  
+
   if (timespan == "All") {
-    df_emo <- df[which(df$class == emotion_lower),]
-  }else{
-    df_emo <- df[which(df$class == emotion_lower & df$Year == timespan),]
+    df_emo <- df[which(df$class == emotion_lower), ]
+  } else {
+    df_emo <- df[which(df$class == emotion_lower & df$Year == timespan), ]
   }
 
   # Make sure the dataframe is not empty
-  if(dim(df_emo)[1] != 0){
-    
+  if (dim(df_emo)[1] != 0) {
     # Extract the text column
-    docs_emo <- iconv(df_emo$text)
-    
+    docs_emo <- iconv(df_emo$processed_text)
+
     # Load the text as a corpus
     docs_emo <- VCorpus(VectorSource(docs_emo))
-    
+
     # Remove ADJ
     docs_emo <- tm_map(docs_emo, remove_adjective_adverb)
-    
-    docs_emo <- tm_map(docs_emo,to_custom,"desk", "frontdesk")
-    docs_emo <- tm_map(docs_emo,to_custom,"check", "check-in/check-out")
-    
+
+    docs_emo <- tm_map(docs_emo, to_custom, "desk", "frontdesk")
+    docs_emo <- tm_map(docs_emo, to_custom, "check", "check-in/check-out")
+
     dtm_emo <- TermDocumentMatrix(docs_emo)
-    
+
     # Convert term doc matrix into matrix
     m_emo <- as.matrix(dtm_emo)
-    
+
     # Sum the frequencies of all words
-    word_freq <- sort(rowSums(m_emo),decreasing = T)
-    
+    word_freq <- sort(rowSums(m_emo), decreasing = T)
+
     # Calculate the total number of unique words
     total_words <- length(word_freq)
-    
+
     # Define the percentage of words to consider as "top words"
-    percentage <- 0.01
-    
-    # This line calculates the number of words to be considered as the "top words". 
-    # The line multiplies "total_words" by "percentage", then rounds the result up 
+    percentage <- 0.05
+
+    # This line calculates the number of words to be considered as the "top words".
+    # The line multiplies "total_words" by "percentage", then rounds the result up
     # to the nearest integer using the "ceiling" function to ensure that at least one word is included in the top words.
-    top_1_percent <- ceiling(total_words * percentage )
-    
-    # This line calculates the frequency threshold below which a word is not considered as one of the top words. 
-    # The "quantile" function takes the "word_freq" vector and the percentage of words to be considered as input. 
+    top_1_percent <- ceiling(total_words * percentage)
+
+    # This line calculates the frequency threshold below which a word is not considered as one of the top words.
+    # The "quantile" function takes the "word_freq" vector and the percentage of words to be considered as input.
     # The percentage value is calculated by subtracting "top_1_percent" from "total_words" and then dividing the result by "total_words".
-    # The resulting value represents the fraction 
-    # of words to be excluded from the top words, so the "quantile" function calculates 
+    # The resulting value represents the fraction
+    # of words to be excluded from the top words, so the "quantile" function calculates
     # the frequency value below which the excluded words fall.
-    freq_threshold <- quantile(word_freq, 1 - top_1_percent/total_words)
-    
+    freq_threshold <- quantile(word_freq, 1 - top_1_percent / total_words)
+
     # Extract the top x% most frequent words
     top_words <- names(word_freq[word_freq >= freq_threshold])
-    
+
     # Convert top_words to data frame
     top_words_df <- data.frame(word = top_words, freq = word_freq[word_freq >= freq_threshold], stringsAsFactors = FALSE)
-    
+
     # Remove NAs from the data frame
     top_words_df <- top_words_df[!is.na(top_words_df$word), ]
-    
-    
+
     # Plot a bar chart of the top 1% most frequent words
     plot <- ggplot(top_words_df, aes(x = reorder(word, -freq), y = freq)) +
       geom_bar(stat = "identity", fill = rainbow(length(top_words_df$word))) +
       geom_text(aes(label = freq), vjust = -0.5, size = 3) +
       theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-      labs(title = paste(hotel_name,"Top", percentage *100, 
-                         "% Most Frequent Words for", emotion,"Comments",
-                         paste("[",data_source,":",timespan,"]",sep = "")), x = "Word", y = "Frequency")
-    
-    ggsave(paste("./Graphs/",emotion_lower,"_",hotel_name,"_",data_source,"_",timespan,".png",sep = ""), plot = plot, width = 13.66, height = 8.68, dpi = 600)
-    
+      labs(title = paste(
+        hotel_name, "Top", percentage * 100,
+        "% Most Frequent Words for", emotion, "Comments",
+        paste("[", data_source, ":", timespan, "]", sep = "")
+      ), x = "Word", y = "Frequency")
+
+    ggsave(paste("./Graphs/", emotion_lower, "_", hotel_name, "_", data_source, "_", timespan, ".png", sep = ""), plot = plot, width = 13.66, height = 8.68, dpi = 600)
   }
-  
 }
 
 
 # Years
-target_year <- c("All","2019","2020","2021","2022","2023")
+target_year <- c("All", "2019", "2020", "2021", "2022", "2023")
 
 # Call the function for each string in the list
 for (i in 1:length(target_year)) {
-  graph_top_words("Positive",target_year[i],"Combined")
-  graph_top_words("Negative",target_year[i],"Combined")
+  graph_top_words("Positive", target_year[i], "Yelp")
+  graph_top_words("Negative", target_year[i], "Yelp")
 }
-
-
